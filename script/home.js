9c24c8edc770b5388b3607ab4d3efb84c67fccd2
Abdur Rauf <abdurrouf3024k@gmail.com>
2026-04-11 21:49:55 +0600

    close button funtionality added

--- script/home.js
+++ script/home.js
@@ -16,11 +16,91 @@ const displaySingleIssue=(words)=>{
  singleContainer.innerHTML = "";
 
 
+
+//  {
+//     "id": 50,
+//     "title": "Create automated testing pipeline",
+//     "description": "Set up CI/CD pipeline with automated tests running on every commit and pull request.",
+//     "status": "open",
+//     "labels": [
+//         "enhancement",
+//         "help wanted"
+//     ],
+//     "priority": "high",
+//     "author": "ci_cd_cindy",
+//     "assignee": "test_tina",
+//     "createdAt": "2024-02-10T08:00:00Z",
+//     "updatedAt": "2024-02-10T08:00:00Z"
+// }
+
   console.log(words);
  const single = document.createElement("div");
 
  single.innerHTML=`
- <p>cat</p>
+  <div class="bg-white shadow-sm rounded-xl py-10 px-5 ">
+
+  <!-- Title -->
+  <h2 class="text-lg font-semibold text-gray-800 mb-2">
+   ${words.title}
+  </h2>
+
+  <!-- Status-->
+  <div class="flex items-center gap-2 text-sm text-gray-500 mb-3">
+    <span class="px-2 py-1 rounded-full text-xs 
+      ${words.status === 'open' ? 'bg-green-100 text-green-600' : 'bg-purple-100 text-purple-600'}">
+      ${words.status.toUpperCase()}
+    </span>
+    <span>• Opened by ${words.author}</span>
+    <span>• ${new Date(words.createdAt).toLocaleDateString()}</span>
+  </div>
+
+  <!-- Labels -->
+   <!-- Labels -->
+    <div class="flex gap-2 mb-3">
+      ${words.labels.map(label => `
+        <span class="px-2 py-1 text-xs rounded-full ${label === 'bug' ? 'bg-red-100 text-red-500' : ''}
+        ${label === 'help wanted' ? 'bg-yellow-100 text-yellow-600' : ''}
+        ${label === 'enhancement' ? 'bg-green-100 text-green-600' : ''}
+        ${label === 'good first issue' ? 'bg-blue-100 text-blue-500' : ''}
+        ${label === 'documentation' ? 'bg-purple-100 text-purple-500' : ''}">
+          ${label}
+        </span>
+      `).join("")}
+    </div>
+
+  <!-- Description -->
+  <p class="text-sm text-gray-600 mb-4">
+    ${words.description}
+  </p>
+
+  <!-- Info -->
+  <div class="bg-gray-100 rounded-lg p-3 flex justify-between mb-4">
+    
+    <div>
+      <p class="text-xs text-gray-500">Assignee:</p>
+      <p class="text-sm font-medium">  ${words.assignee || "Not assigned"}</p>
+    </div>
+
+    <div>
+      <p class="text-xs text-gray-500">Priority:</p>
+      <span class="px-2 py-1 rounded-full text-xs   ${words.priority === 'high' ? 'bg-red-500 text-white' : ''}
+        ${words.priority === 'medium' ? 'bg-yellow-500 text-white' : ''}
+        ${words.priority === 'low' ? 'bg-gray-400 text-white' : ''}">
+          ${words.priority}
+        
+      </span>
+    </div>
+
+  </div>
+
+  <!-- Button -->
+  <div class="flex justify-end">
+    <button class="bg-purple-600 text-white px-4 py-1 rounded">
+      Close
+    </button>
+  </div>
+
+</div>
  `;
  singleContainer.append(single);
 
@@ -45,7 +125,8 @@ issueDiv.innerHTML = `
 ${issue.status === 'open'?'border-[#00A96E]' : 'border-[#A855F7]'} 
 p-4 flex flex-col justify-between h-full
  ">
- <button onclick = "loadSingleIssue(${issue.id})" class = "btn"></button>
+ issueDiv.setAttribute("onclick", `loadSingleIssue(${issue.id})`);
+issueDiv.classList.add("cursor-pointer");
   <!-- Top section -->
   <div>
     <!-- Priority -->
